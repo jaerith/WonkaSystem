ec05e49e7148cfa294294d805f2d51649714f6aa
jaerith <Aaron.Kendall@gmail.com>
2017-10-09 16:39:15 -0400

    Initial revision.

---
+++ WonkaRef/WonkaRefAttr.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace PmdRef
+{
+    /// <summary>
+    /// 
+    /// This class represents a distinct data point within the Wonka System 
+    /// (like UPC, Title, PubPrice, etc.)
+    ///     
+    /// </summary>
+    [DataContract(Namespace = "http://sample.wonkasystem.com")]
+    [XmlType(TypeName = "Attr")]
+    public class WonkaRefAttr
+    {
+        public WonkaRefAttr()
+        {
+            AttrId = -1;
+
+            AttrName = TabCol = DisplayName = null;
+            TabName = ColName = Description = null;
+
+            FieldId = GroupId = MaxLength = -1;
+
+            Precision = Scale = -1;
+
+            DefaultValue = MinValue = MaxValue = AttrModDt = null;
+
+            MaxLengthTruncate = false;
+            AttrModDtFlag     = false;
+
+            Properties = new HashSet<string>();
+
+            IsAudited        = IsBookInHand = IsDate = IsDecimal = false;
+            IsFileImportAble = IsMaintainable = IsNumeric = IsRefCd = false;
+
+            RefCdDomainAttrName = null;
+        }
+
+        [DataMember]
+        public int AttrId { get; set; }
+
+        [DataMember]
+        public string AttrName { get; set; }
+
+        [DataMember]
+        public string TabCol { get; set; }
+
+        [DataMember]
+        public string DisplayName { get; set; }
+
+        [DataMember]
+        public string TabName { get; set; }
+
+        [DataMember]
+        public string ColName { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public int FieldId { get; set; }
+
+        [DataMember]
+        public int GroupId { get; set; }
+
+        [DataMember]
+        public int MaxLength { get; set; }
+
+        [DataMember]
+        public bool MaxLengthTruncate { get; set; }
+
+        [DataMember]
+        public int Precision { get; set; }
+
+        [DataMember]
+        public int Scale { get; set; }
+
+        [DataMember]
+        public string DefaultValue { get; set; }
+
+        [DataMember]
+        public string MinValue { get; set; }
+
+        [DataMember]
+        public string MaxValue { get; set; }
+
+        [DataMember]
+        public string AttrModDt { get; set; }
+
+        [DataMember]
+        public bool AttrModDtFlag { get; set; }
+
+        [DataMember]
+        public HashSet<string> Properties { get; set; }
+
+        [DataMember]
+        public bool IsDate { get; set; }
+
+        [DataMember]
+        public bool IsDecimal { get; set; }
+
+        [DataMember]
+        public bool IsNumeric { get; set; }
+
+        [DataMember]
+        public bool IsRefCd { get; set; }
+
+        [DataMember]
+        public bool IsMaintainable { get; set; }
+
+        [DataMember]
+        public bool IsBookInHand { get; set; }
+
+        [DataMember]
+        public bool IsFileImportAble { get; set; }
+
+        [DataMember]
+        public bool IsAudited { get; set; }
+
+        [DataMember]
+        public string RefCdDomainAttrName { get; set; }
+    }
+}
