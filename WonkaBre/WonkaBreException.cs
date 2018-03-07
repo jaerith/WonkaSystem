58f6d91e01ff8ecd99539efa28c93e5e29174195
jaerith <Aaron.Kendall@gmail.com>
2018-03-07 14:12:36 -0500

    Initial revision

---
+++ WonkaBre/WonkaBreException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WonkaBre
+{
+    /// <summary>
+    /// 
+    /// This exception should be used when encountering any issue with using
+    /// the Business Rules Engine.
+    /// 
+    /// </summary>
+    class WonkaBreException : Exception
+    {
+        public WonkaBreException(int pnRuleSetId, int pnRuleId, string psErrorMessage)
+        {
+            RuleSetId = pnRuleSetId;
+            RuleId    = pnRuleId;
+            Msg       = psErrorMessage;
+        }
+
+        public int RuleSetId { get; }
+
+        public int RuleId { get; }
+
+        public string Msg { get; }
+    }
+}
