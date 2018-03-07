7cf9385052eb84e7cb9f19c7fcfee95d9167f1c8
jaerith <Aaron.Kendall@gmail.com>
2018-03-07 12:58:36 -0500

    Added code to populate data for the test harness.

--- WonkaSystem/TestHarness/WonkaMetadataTestSource.cs
+++ WonkaSystem/TestHarness/WonkaMetadataTestSource.cs
@@ -19,12 +19,12 @@ namespace WonkaSystem.TestHarness
         {
             List<WonkaRefAttr> AttrCache = new List<WonkaRefAttr>();
 
-            AttrCache.Add(new WonkaRefAttr() { AttrId = 1, AttrName = "BankAccountID",   FieldId = 101, GroupId = 1, IsAudited = false, IsNumeric = true });
-            AttrCache.Add(new WonkaRefAttr() { AttrId = 2, AttrName = "BankAccoutName",  FieldId = 102, GroupId = 1, IsAudited = true,  MaxLength = 1024 });
-            AttrCache.Add(new WonkaRefAttr() { AttrId = 3, AttrName = "AccountType",     FieldId = 2,   GroupId = 2, IsAudited = false, MaxLength = 1024 });
-            AttrCache.Add(new WonkaRefAttr() { AttrId = 4, AttrName = "AccountTypeRank", FieldId = 2,   GroupId = 2, IsAudited = true,  MaxLength = 1024 });
-            AttrCache.Add(new WonkaRefAttr() { AttrId = 5, AttrName = "BankAccoutName",  FieldId = 2,   GroupId = 2, IsAudited = true,  MaxLength = 1024 });
-            AttrCache.Add(new WonkaRefAttr() { AttrId = 6, AttrName = "BankAccoutName",  FieldId = 2,   GroupId = 2, IsAudited = true,  MaxLength = 1024 });
+            AttrCache.Add(new WonkaRefAttr() { AttrId = 1, AttrName = "BankAccountID",    FieldId = 101, GroupId = 1, IsAudited = false, IsNumeric = true });
+            AttrCache.Add(new WonkaRefAttr() { AttrId = 2, AttrName = "BankAccoutName",   FieldId = 102, GroupId = 1, IsAudited = true,  MaxLength = 1024 });
+            AttrCache.Add(new WonkaRefAttr() { AttrId = 3, AttrName = "AccountType",      FieldId = 2,   GroupId = 2, IsAudited = false, MaxLength = 1024 });
+            AttrCache.Add(new WonkaRefAttr() { AttrId = 4, AttrName = "AccountRank",      FieldId = 2,   GroupId = 2, IsAudited = true,  IsNumeric = true, MaxValue = 256 });
+            AttrCache.Add(new WonkaRefAttr() { AttrId = 5, AttrName = "AccountCurrency",  FieldId = 2,   GroupId = 2, IsAudited = true,  MaxLength = 3 });
+            AttrCache.Add(new WonkaRefAttr() { AttrId = 6, AttrName = "AccountCurrValue", FieldId = 2,   GroupId = 2, IsAudited = true,  IsDecimal = true });
 
             return AttrCache;
         }
@@ -33,12 +33,22 @@ namespace WonkaSystem.TestHarness
         {
             List<WonkaRefCurrency> CurrencyCache = new List<WonkaRefCurrency>();
 
+            CurrencyCache.Add(new WonkaRefCurrency() { CurrencyId = 1, CurrencyCd = "USD", USDCost = 1,        USDList = 1 });
+            CurrencyCache.Add(new WonkaRefCurrency() { CurrencyId = 2, CurrencyCd = "EUR", USDCost = 1.24f,    USDList = 1.24f });
+            CurrencyCache.Add(new WonkaRefCurrency() { CurrencyId = 3, CurrencyCd = "CNY", USDCost = 0.16f,    USDList = 0.16f });
+            CurrencyCache.Add(new WonkaRefCurrency() { CurrencyId = 4, CurrencyCd = "BTC", USDCost = 9722.73f, USDList = 9722.73f });
+            CurrencyCache.Add(new WonkaRefCurrency() { CurrencyId = 5, CurrencyCd = "USD", USDCost = 811.68f,  USDList = 811.68f });
+
             return CurrencyCache;
         }
 
         public List<WonkaRefField> GetFieldCache()
         {
             List<WonkaRefField> FieldCache = new List<WonkaRefField>();
+
+            FieldCache.Add(new WonkaRefField() { FieldId = 101, FieldName = "BankAccountID",  GroupId = 1, AttrIds = new List<int>(){1} });
+            FieldCache.Add(new WonkaRefField() { FieldId = 102, FieldName = "BankAccoutName", GroupId = 1, AttrIds = new List<int>(){2} });
+            FieldCache.Add(new WonkaRefField() { FieldId = 2,   FieldName = "Account",        GroupId = 2, AttrIds = new List<int>(){3,4,5} });
 
             return FieldCache;
         }
@@ -47,6 +57,9 @@ namespace WonkaSystem.TestHarness
         {
             List<WonkaRefGroup> GroupCache = new List<WonkaRefGroup>();
 
+            GroupCache.Add(new WonkaRefGroup() { GroupId = 1, GroupName = "MainAccount", Description = "The main account to which all accounts tie into" });
+            GroupCache.Add(new WonkaRefGroup() { GroupId = 2, GroupName = "Account", IsSequenced = true, Description = "A consumer account of a particular type and currency" });
+
             return GroupCache;
         }
 
@@ -54,12 +67,20 @@ namespace WonkaSystem.TestHarness
         {
             List<WonkaRefSource> SourceCache = new List<WonkaRefSource>();
 
+            SourceCache.Add(new WonkaRefSource() { SourceId = 1, SourceName = "TransUnion", Status = "ACT" });
+            SourceCache.Add(new WonkaRefSource() { SourceId = 2, SourceName = "Experian",   Status = "ACT" });
+
             return SourceCache;
         }
 
         public List<WonkaRefSourceField> GetSourceFieldCache()
         {
             List<WonkaRefSourceField> SourceFieldCache = new List<WonkaRefSourceField>();
+
+            SourceFieldCache.Add(new WonkaRefSourceField() { SourceFieldId = 1, SourceId = 1, FieldId = 102, SecurityLevel = 3 });
+            SourceFieldCache.Add(new WonkaRefSourceField() { SourceFieldId = 2, SourceId = 1, FieldId = 2,   SecurityLevel = 1 });
+            SourceFieldCache.Add(new WonkaRefSourceField() { SourceFieldId = 3, SourceId = 2, FieldId = 102, SecurityLevel = 3 });
+            SourceFieldCache.Add(new WonkaRefSourceField() { SourceFieldId = 4, SourceId = 2, FieldId = 2,   SecurityLevel = 1 });
 
             return SourceFieldCache;
         }
--- WonkaSystem/TestHarness/WonkaSimpleTest.cs
+++ WonkaSystem/TestHarness/WonkaSimpleTest.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using WonkaPrd;
+using WonkaRef;
+
 namespace WonkaSystem.TestHarness
 {
     public class WonkaSimpleTest
     {
+
         public WonkaSimpleTest()
         {
+        }
+
+        public void Execute()
+        {
+            WonkaRefEnvironment RefEnv =
+                WonkaRefEnvironment.CreateInstance(false, new WonkaMetadataTestSource());
+
+            // Cue the rules engine
         }
     }
 }
