7f853f7c1a8ca7b694e40db4bf5288d13c47d029
jaerith <Aaron.Kendall@gmail.com>
2018-03-08 11:32:29 -0500

    Added code to make use of the test harness.

--- WonkaSystem/Program.cs
+++ WonkaSystem/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using WonkaSystem.TestHarness;
+
 namespace WonkaSystem
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                WonkaSimpleTest SimpleTest = new WonkaSimpleTest();
+
+                SimpleTest.Execute();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+
+            return;
         }
     }
 }
